0e5435b131cad197b90ac3001a25d6b8aab92f91
Elizabeth Rodriguez Henry <elirhenry@Elizabeths-iMac.local>
2024-04-16 12:39:57 -0600

    established connection after changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
+import Home from './Home';
+// import Login from '/Login.js';
+// import Register from '/Register.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path={'/home'} element={<Home/>}></Route>
+          {/* <Route path={'/login'} element={<Login/>}></Route>
+          <Route path={'/register'} element={<Register/>}></Route> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
